05c53ef580219b4d6e964a6729c30d1e8bdde0a4
Safalgautam3636 <seeram3636@gmail.com>
2024-01-30 02:20:23 -0500

    add Car

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,11 @@
 public class Car extends Vehicle{
-    
+    String modelNumber;
+
+    void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    String getModelNumber() {
+        return this.modelNumber;
+    }
 }
